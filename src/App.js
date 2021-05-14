57d35d44c4a969a18b441a685c446f9a6a826662
Sumitra-1810 <sumitrathawani899@gmail.com>
2021-05-14 10:46:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>
+{
+  let hours=new Date().getHours();
+  
+  let current= new Date().toLocaleTimeString();
+  let meridiem="";
+  const[time,setTime]=useState(current);
+  const updateTime=()=>
+  {
+    current= new Date().toLocaleTimeString();
+    
+    setTime(current);
+    
+  }
+  if(hours<12)
+  {
+    meridiem="AM"
+  }
+  else{
+    meridiem="PM"
+  }
+  setInterval(updateTime,1000);
+  return(
+    <>
+    
+    <div className="main">
+     
+    <div className="clock">
+    <h1> Digital Clock</h1>
+      {time} {meridiem}</div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
